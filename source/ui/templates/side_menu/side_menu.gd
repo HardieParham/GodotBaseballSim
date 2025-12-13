a98cf478f18a345e3392ce2f5fe316fb3b518feb
Hardie Parham <78958134+HardieParham@users.noreply.github.com>
2025-12-13 17:14:51 -0600

    [ui] main menu formatting

---
+++ source/ui/templates/side_menu/side_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+@onready var general_label: Label = %GeneralLabel
+@onready var league_button: Button = %LeagueButton
+@onready var team_button: Button = %TeamButton
+@onready var game_label: Label = %GameLabel
+@onready var strategy_button: Button = %StrategyButton
+@onready var roll_button: Button = %RollButton
+
+
+func _on_league_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_team_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_strategy_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_roll_button_pressed() -> void:
+	pass # Replace with function body.
